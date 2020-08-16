bdd62a95616130399667e1be77695de8d66bbc59
MarqCervMartin <martin_mcervantes@hotmail.com>
2020-08-15 23:49:15 -0500

    feat: full session terminate and views with secure session

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,42 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
+const {isLoggedIn} = require('../lib/auth');
 
-router.get('/add', (req, res ) =>{
+
+router.get('/add', isLoggedIn, (req, res ) =>{
     res.render('links/add');
 });
 
-router.post('/add', async(req, res) =>{
+router.post('/add', isLoggedIn, async(req, res) =>{
     const {nombre, cantidad, mensaje} = req.body;
     const newPedido = {
         nombre,
         cantidad,
-        mensaje
+        mensaje,
+        user_id: req.user.id
     };
     await pool.query('INSERT INTO pedidos set ?',[newPedido]);
     req.flash('success', 'Guardado correctamente');
     res.redirect('/links');
 });
 
-router.get('/', async(req, res) =>{
-    const pedidos = await pool.query('SELECT * FROM pedidos');
+router.get('/', isLoggedIn, async(req, res) =>{
+    const pedidos = await pool.query('SELECT * FROM pedidos WHERE user_id = ?',[req.user.id]);
     res.render('links/list.hbs', {pedidos});
 });
 
-router.get('/delete/:id', async(req, res) =>{
+router.get('/delete/:id', isLoggedIn, async(req, res) =>{
     const {id} = req.params;
     await pool.query('DELETE FROM pedidos WHERE ID = ?', [id]);
     req.flash('success','Pedido eliminado satisfactoriamente');
     res.redirect('/links');
 });
 
-router.get('/edit/:id', async(req, res) =>{
+router.get('/edit/:id', isLoggedIn, async(req, res) =>{
     const {id} = req.params;
     const pedido = await pool.query('SELECT * FROM pedidos WHERE ID = ?', [id]);
     res.render('links/edit',{pedido: pedido[0]});
 });
 
-router.post('/edit/:id', async(req, res) =>{
+router.post('/edit/:id', isLoggedIn, async(req, res) =>{
     const {id} = req.params;
     const {nombre, cantidad, mensaje} = req.body;
     const newPedido = {
